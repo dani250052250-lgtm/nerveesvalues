9cb4d830a8b376829ea6035a335914bf13563ed5
Nervees <dani250052250@gmail.com>
2026-07-11 04:08:42 +0500

    Update favorites.js

--- js/favorites.js
+++ js/favorites.js
@@ -1,1 +1,191 @@
+// ❤️ Nervees Values
+// Favorites System
 
+
+let favorites = 
+JSON.parse(
+localStorage.getItem("favorites")
+)
+||
+[];
+
+
+
+
+
+
+// Добавление в избранное
+
+
+function addFavorite(item){
+
+
+    let exists =
+    favorites.find(
+    fav => fav.id === item.id
+    );
+
+
+
+    if(!exists){
+
+
+        favorites.push(item);
+
+
+
+        saveFavorites();
+
+
+
+        alert(
+        "❤️ Added to favorites"
+        );
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+// Удаление
+
+
+function removeFavorite(id){
+
+
+
+favorites =
+favorites.filter(
+item=>item.id !== id
+);
+
+
+
+saveFavorites();
+
+
+renderFavorites();
+
+
+
+}
+
+
+
+
+
+
+
+// Сохранение
+
+
+function saveFavorites(){
+
+
+localStorage.setItem(
+
+"favorites",
+
+JSON.stringify(favorites)
+
+);
+
+
+}
+
+
+
+
+
+
+
+// Отображение
+
+
+function renderFavorites(){
+
+
+
+let box =
+document.getElementById(
+"favorites"
+);
+
+
+
+if(!box)
+
+return;
+
+
+
+
+box.innerHTML = "";
+
+
+
+
+
+favorites.forEach(item=>{
+
+
+
+box.innerHTML += `
+
+
+<div class="card">
+
+
+<img src="images/pets/${item.image}">
+
+
+
+<h3>
+
+${item.name}
+
+</h3>
+
+
+
+<button onclick="removeFavorite('${item.id}')">
+
+❌ Remove
+
+</button>
+
+
+
+</div>
+
+
+
+`;
+
+
+
+});
+
+
+
+}
+
+
+
+
+
+
+
+// Запуск
+
+
+renderFavorites();
